24c34eaa25238f7e503ae335c9830667ea7555be
Beth Dellea <bethdellea@gmail.com>
2016-10-24 12:44:28 -0400

    Slowly progressing towards the goal
    Getting closer every day

--- tagTrends/trendsExtension/bg.js
+++ tagTrends/trendsExtension/bg.js
@@ -1,38 +1,37 @@
-//chrome.tabs.executeScript({ file: 'jquery-2.1.3.js'}); 
+chrome.tabs.executeScript({ file: 'jquery-2.1.3.js'}); 
 //document.getElementById('GNsubmit').addEventListener('click', neutralize);
 
 //if we're on AO3 author page or IG user page (@ in the title of the page, along wiht url check)
 //run the code --- if ig, get pic codes first, if AO3 just get uname and pseud
 
+function getTagsA(){
+    chrome.tabs.executeScript({file: 'a.js'});
+    console.log("tag a executed");
+}
 
+function getTagsI(){
+    chrome.tabs.executeScript({file: 'i.js'});
+    console.log("tag i executed");
+}
 
+function checkUrl(urlIn){
+    //RE with urlIn to check for A or I
+    var urlPieces = urlIn.split("/");
     
-/*    
-function neutralize(){
-    var pro = $('input[name=pronoun]:checked', '#choosePls').val(); 
-    console.log(pro);
-    //alert(pro);
-    
-    //calls a different .js file for each pronoun choice
-    //the evaluation is done on a page-by-page level, not directly in this file for the extension
-    if (pro == "m"){ 
-        console.log("m chosen");
-        chrome.tabs.executeScript( { file: 'm.js'}); 
-        console.log("m executed");
-    } else{
-        if (pro == "f"){ 
-            console.log("f chosen");
-            chrome.tabs.executeScript( { file: 'f.js' }); 
-            console.log("f executed");
-        } else{ 
-            if (pro == "t") { 
-                console.log("t chosen");
-                chrome.tabs.executeScript( { file: 't.js' }); 
-                console.log("t executed");
-            }     
-        }
+    console.log(urlPieces);
+}
+
+ var query = {active: true, currentWindow: true};
+function callback(tabs){
+    var currentTab = tabs[0];
+    var currURL = currentTab.url;
+    var curr = checkUrl(currURL);
+    if( curr == "a"){
+        getTagsA();
     }
-    
-    
-};
-*/
+    if(curr == "i"){
+        getTagsI();
+    }
+}
+
+chrome.tabs.query(query, callback);
