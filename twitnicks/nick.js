e8640cc5f782f4391299d79dd38b5b2b32a32c25
Beth Dellea <bethdellea@gmail.com>
2017-12-31 13:51:19 -0500

    v1.O
    works with twitter website, updates on ajax loads of more tweets

--- twitnicks/nick.js
+++ twitnicks/nick.js
@@ -1,45 +1,51 @@
-//is this twitter or tweetdeck?? (or neither :o)
-
- //chrome.tabs.onActivated.addListener(function(evt){ 
-/*
- chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
-
-     // since only one tab should be active and in the current window at once
-     // the return variable should only have one entry
-     var activeTab = arrayOfTabs[0];
-     var activeTabId = activeTab.id; // or do whatever you need
-	 //if(activeTab.url.indexOf("twitter.com") > -1){
-		alert("a twit page");
-		alert(activeTab.url);
-		//}
-  });  //possibly can just do window.location.href for this...... will need to test and check when I'm not busy being a scardey cat
-*/
-
 var currTab = window.location.href;
-//alert(currTab);
-//currently unnecessary check due to the things going on in manifest.json
- 
+console.log(currTab);
 //do need to set things up to also run on page loads/document changes, since twitter likes to ajax things in and also because it's good to cover bases
   
   
 //load up saved data
-//var nickPairs = JSON.parse(localStorage.getItem("twitNicData"));
-chrome.storage.sync.get("twitNicData", processNics);
-	//var nicks = {};
+//will happen on initial page load...
+function getNicks(){
+    chrome.storage.sync.get("twitNicData", processNics);
+}
+
 function processNics(nickPairs){
-	
-	if(!$.isEmptyObject(nickPairs)){
+    if(!$.isEmptyObject(nickPairs)){
 		var pairedNics = JSON.parse(nickPairs["twitNicData"]);
-		console.log("I'm an thing!! do things withe me!!!");
-		$.each(pairedNics, function(username, nickname){
-			console.log("I just processed " + username + " to the array under the alias " + nickname + "!!!");
-		});
+        if(!$.isEmptyObject(pairedNics)){
+	        //do replacement things,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+            $.each(pairedNics, function(username, nickname){
+                sortOutTweets(nickname, username);
+            });
+        }
 	}
-
-if(!$.isEmptyObject(pairedNics)){
-	//do replacement things,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-
-
 }
 
+function sortOutTweets(nick, handle){
+    handle = "@" + handle;
+    $("div.tweet").each(function(){
+        if(!$(this).hasClass("nickProcessed")){
+            var tweetAuth = $(".account-group .username", this).text();
+            if(tweetAuth == handle){
+                var dispName = $(".fullname",this).text();
+                dispName += " (" + nick +")";
+                $(".fullname", this).text(dispName);
+                $(this).addClass("nickProcessed"); 
+            }
+        }
+       
+    }); 
 }
+
+getNicks();
+
+function DOMModificationHandler(){
+    $(this).unbind('DOMSubtreeModified');
+    setTimeout(function(){
+        getNicks();
+        $('#page-container').bind('DOMSubtreeModified',DOMModificationHandler);
+    },2500);
+}
+
+//after document-load
+$('#page-container').bind('DOMSubtreeModified',DOMModificationHandler);
